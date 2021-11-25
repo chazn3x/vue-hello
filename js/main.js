4ffcfba3224b3de14c9e65914cd105fd472760b5
Giovanni <gio.a.98@gmail.com>
2021-11-25 17:33:58 +0100

    funzione random in methods

--- js/main.js
+++ js/main.js
@@ -2,15 +2,14 @@ const app = new Vue({
     el: "#root",
     data: {
         text: "Hello",
-        img: `https://picsum.photos/id/${randomNum()}/200/300`
+        img: `https://picsum.photos/id/99/200/300`
     },
     methods: {
+        randomNum: function() {
+            return Math.floor(Math.random() * 1000) + 1;
+        },
         changeImg: function () {
-            this.img = `https://picsum.photos/id/${randomNum()}/200/300`;
+            this.img = `https://picsum.photos/id/${this.randomNum()}/200/300`;
         }
     }
 });
-function randomNum() {
-    const num = Math.floor(Math.random() * 1000) + 1;
-    return num;
-}
