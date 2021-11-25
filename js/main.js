752eed26b2109d23bf30b8a2b8da542f7e7d88de
Giovanni <gio.a.98@gmail.com>
2021-11-25 13:51:00 +0100

    button per cambio immagine dinamico

--- js/main.js
+++ js/main.js
@@ -2,6 +2,15 @@ const app = new Vue({
     el: "#root",
     data: {
         text: "Hello",
-        img: "https://picsum.photos/id/237/200/300"
+        img: `https://picsum.photos/id/${randomNum()}/200/300`
+    },
+    methods: {
+        changeImg: function () {
+            this.img = `https://picsum.photos/id/${randomNum()}/200/300`;
+        }
     }
 });
+function randomNum() {
+    const num = Math.floor(Math.random() * 1000) + 1;
+    return num;
+}
